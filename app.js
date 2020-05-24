e6bc3fe88d1487085bdd827d03e7a0e253d82995
Nguyen Thien Tam <tamtam132@gmail.com>
2020-05-24 13:11:34 +0200

    Modified addTodo function in app.js. Now we can add list item to our list.

--- app.js
+++ app.js
@@ -13,5 +13,28 @@ function addTodo(event)
     // Prevent form from submitting.
     event.preventDefault();
     
-    
+    // Todo DIV
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    // Create LI
+    const newTodo = document.createElement("li");
+    newTodo.innerText = "new list item";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    // CHECK MARK BUTTON
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML = "<i class='fas fa-check'></i>";
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    // CHECK TRASH BUTTON
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
 }
