8022bf0f852ac73bf801a25e7630ef2233273354
Nguyen Thien Tam <tamtam132@gmail.com>
2020-06-01 21:40:48 +0200

    Finished todo webapp. There are small bugs, but for now I leave them there. Later I will fix it.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const todoList = document.querySelector(".todo-list");
 const filterOption = document.querySelector(".filter-todo");
 
 //____________Event Listeners_______________
+document.addEventListener("DOMContentLoaded", getTodos);
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", checkDelete);
 filterOption.addEventListener("click", filterTodo);
@@ -25,6 +26,9 @@ function addTodo(event)
     newTodo.innerText = todoInput.value;
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
+
+    // ADD TODO  TO LOCAL STORAGE
+    saveLocalTodos(todoInput.value);
 
     // CHECK MARK BUTTON
     const completeButton = document.createElement("button");
@@ -65,6 +69,9 @@ function checkDelete(e)
         
         // Animation
         todo.classList.add("fall");
+
+        // Delete from local storage
+        removeLocalTodos(todo);
 
         // After the transition remove item
         todo.addEventListener("transitionend", function()
@@ -109,7 +116,87 @@ function filterTodo(e)
     });
 }
 
+// Save items to local storage
 function saveLocalTodos(todo)
 {
-    
+    let todos;
+
+    // Check if we already have an array in local storage. If not then we make an empty array, otherwise we access and save the local storage data in todos.
+    if(localStorage.getItem("todos") === null)
+    {
+        todos = [];
+    }
+    else
+    {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    // We push the new data to our array and then push it back to local storage.
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// After refresh the page we query the data from local storage and update todo list.
+function getTodos()
+{
+    let todos;
+
+    // Check if we already have an array in local storage. If not then we make an empty array, otherwise we access and save the local storage data in todos.
+    if(localStorage.getItem("todos") === null)
+    {
+        todos = [];
+    }
+    else
+    {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    todos.forEach(function(todo)
+    {
+        // Todo DIV
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+
+        // Create LI
+        const newTodo = document.createElement("li");
+        newTodo.innerText = todo;
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+
+        // CHECK MARK BUTTON
+        const completeButton = document.createElement("button");
+        completeButton.innerHTML = "<i class='fas fa-check'></i>";
+        completeButton.classList.add("complete-btn");
+        todoDiv.appendChild(completeButton);
+
+        // CHECK TRASH BUTTON
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = "<i class='fas fa-trash'></i>";
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        // APPEND TO LIST
+        todoList.appendChild(todoDiv);
+    });
+}
+
+// Remove item from local storage
+function removeLocalTodos(todo)
+{
+    let todos;
+
+    // Check if we already have an array in local storage. If not then we make an empty array, otherwise we access and save the local storage data in todos.
+    if(localStorage.getItem("todos") === null)
+    {
+        todos = [];
+    }
+    else
+    {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+
+    localStorage.setItem("todos", JSON.stringify(todos));
 }
